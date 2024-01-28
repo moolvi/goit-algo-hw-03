1b98571fb8697b16b238ad04f6bbb40a1f1d086a
Oleksandr <sanelka77@yahoo.com>
2024-01-28 21:32:36 +0200

    difference between dates with corrections

--- task1.py
+++ task1.py
@@ -2,6 +2,6 @@ from datetime import datetime
 
 def get_days_from_today(date):
     format = '%Y-%m-%d'
-    date_custom = datetime.strptime(date, format).today()
-    now_date = datetime.now().today()
-    return (now_date - date_custom).days()
+    date_custom = datetime.strptime(date, format)
+    now_date = datetime.today()
+    return (now_date - date_custom).days
